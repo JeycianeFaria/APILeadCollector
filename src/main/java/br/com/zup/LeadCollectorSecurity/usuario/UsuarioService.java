66c1559a4213703a43244678292c0170438fd8f1
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-08 19:04:37 -0300

    Criptografar senha

--- src/main/java/br/com/zup/LeadCollectorSecurity/usuario/UsuarioService.java
+++ src/main/java/br/com/zup/LeadCollectorSecurity/usuario/UsuarioService.java
@@ -1,6 +1,7 @@
 package br.com.zup.LeadCollectorSecurity.usuario;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,9 +9,12 @@ public class UsuarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+    @Autowired
+    private BCryptPasswordEncoder encoder;
 
 
     public Usuario salvarUsuario(Usuario usuario){
+        usuario.setSenha(encoder.encode(usuario.getSenha()));
         return usuarioRepository.save(usuario);
     }
 
