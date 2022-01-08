09daf3e9e217de99f291ebe5b651d0646d95f05f
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-08 16:51:30 -0300

    Criar classe de configuração de segurança

---
+++ src/main/java/br/com/zup/LeadCollectorSecurity/config/security/ConfiguracaoDeSeguranca.java
@@ -0,0 +1,10 @@
+package br.com.zup.LeadCollectorSecurity.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
+}
