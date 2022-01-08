7c0335d08e07ca30e76d3d90ee999fa1d01e6020
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-08 20:58:13 -0300

    Usuario Login Service

--- src/main/java/br/com/zup/LeadCollectorSecurity/usuario/UsuarioRepository.java
+++ src/main/java/br/com/zup/LeadCollectorSecurity/usuario/UsuarioRepository.java
@@ -2,8 +2,11 @@ package br.com.zup.LeadCollectorSecurity.usuario;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, UUID> {
+
+    Optional<Usuario> findByEmail();
 
 }
